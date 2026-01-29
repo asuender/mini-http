6640f753a36623a17b5930524e04be8e9c423168
Andreas Sünder <andreas.suender@icloud.com>
2026-01-29 23:11:55 +0100

    feat: add verbose mode

--- main.c
+++ main.c
@@ -1,23 +1,45 @@
-#include "src/server.h"
+#include "src/socket.h"
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <strings.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-void error(const char *msg) { perror(msg); }
-
-void error_exit(const char *msg) {
-  error(msg);
-  exit(1);
-}
-
 #define PORT 8080
 #define SERVER_RESPONSE "HTTP/1.1 501 Not Implemented\nConnection: close\n"
 
+#define log(fmt, ...)                                                          \
+  do {                                                                         \
+    if (mode == MODE_VERBOSE) {                                                \
+      printf("* " fmt "\n", ##__VA_ARGS__);                                    \
+    }                                                                          \
+  } while (0)
+#define error(msg) perror(msg);
+#define error_exit(msg)                                                        \
+  perror(msg);                                                                 \
+  exit(EXIT_FAILURE);
+
+typedef enum { MODE_NORMAL, MODE_VERBOSE } server_mode;
+
 int main(int argc, char *argv[]) {
+  /* CLI parsing */
+  server_mode mode;
+
+  // I know that supporting verbose mode only doesn't necessarily require
+  // a for loop, but let's keep this for potential flags in the future
+  for (size_t optind = 1; optind < argc && argv[optind][0] == '-'; optind++) {
+    switch (argv[optind][1]) {
+    case 'v':
+      mode = MODE_VERBOSE;
+      break;
+    default:
+      mode = MODE_NORMAL;
+    }
+  }
+
   int sockfd, acc_sockfd, n;
   char buffer[1024];
   socklen_t client_len;
@@ -43,7 +65,17 @@ int main(int argc, char *argv[]) {
   if (n < 0) {
     error_exit("Error reading from socket");
   }
-  printf("Received message from client:\n%s", buffer);
+
+  // Print each header line in verbose mode
+  char *line = buffer;
+  char *crlf;
+  while ((crlf = strstr(line, "\r\n")) != NULL) {
+    // empty line marks end of headers
+    if (crlf == line)
+      break;
+    log("%.*s", (int)(crlf - line), line);
+    line = crlf + 2;
+  }
 
   n = write(acc_sockfd, SERVER_RESPONSE, strlen(SERVER_RESPONSE) + 1);
   if (n < 0) {
